f7ae12726ab92e9b344cfa9519187af9e2c71f42
Chuck Bryan <swampyfox@gmail.com>
2024-03-21 07:28:09 -0400

    updated service health

---
+++ Service1/Program.cs
@@ -0,0 +1,36 @@
+using RabbitMQ.Client;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://localhost:5000");
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+string host = builder.Configuration["RABBITMQ_HOST"] ??
+              throw new InvalidOperationException("RABBITMQ_HOST configuration is invalid");
+string port = builder.Configuration["RABBITMQ_PORT"]??
+              throw new InvalidOperationException("RABBITMQ_PORT configuration is invalid");
+string pass = builder.Configuration["RABBITMQ_PASS"] ??
+              throw new InvalidOperationException("RABBITMQ_USER configuration is invalid");
+string user = builder.Configuration["RABBITMQ_USER"]??
+              throw new InvalidOperationException("RABBITMQ_PASS configuration is invalid");
+
+// Add health check services: https://www.nuget.org/packages/AspNetCore.HealthChecks.Rabbitmq/
+builder.Services
+    .AddHealthChecks()
+    .AddRabbitMQ(rabbitConnectionString:$"amqp://{user}:{pass}@{host}:{port}/");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Map a health endpoint
+app.MapHealthChecks("/health").WithName("Health").WithOpenApi();
+
+app.Run();
